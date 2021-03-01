21d0468986d986a71d0cdad0befaee24cb569cdf
Dennis Wong <denniswong0106@gmail.com>
2021-03-01 22:26:02 +0000

    created a function that compares two given values and returns whether the two given values are equal or not.

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log ('🤟🤟🤟Assertion Passed: ' + actual + ' === ' + expected)
+  } else {
+    console.log ('😡😡😡Assertion Failed: ' + actual + ' !== ' + expected)
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 'one');
+assertEqual('why', 'why');
+assertEqual(2 , 4);
