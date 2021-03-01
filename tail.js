ebb4daaf4a669a42aa03dec85fb7144505d06376
Dennis Wong <denniswong0106@gmail.com>
2021-03-01 23:33:35 +0000

    eslint formatting tail.js

--- tail.js
+++ tail.js
@@ -11,14 +11,14 @@ const tail = array => {
   return array.slice(1);
 };
 
-assertEqualArray = (actualArr, expectedArr) => {
-  if (actualArr.length === 0 && expectedArr.length === 0){
+const assertEqualArray = (actualArr, expectedArr) => {
+  if (actualArr.length === 0 && expectedArr.length === 0) {
     console.log(`🤟🤟🤟Assertion Passed: Empty === Empty`);
   } else {
-    for (let i = 0; i < actualArr.length; i++){
-      if (actualArr[i] !== expectedArr[i]){
+    for (let i = 0; i < actualArr.length; i++) {
+      if (actualArr[i] !== expectedArr[i]) {
         console.log(`😡😡😡Assertion Failed: at index ${i}, ${actualArr[i]} !== ${expectedArr[i]}`);
-        break; 
+        break;
         // so if the array has 50 wrong comparisons it won't just keep going.
         // stops code running once first error is detected (leftmost side).
       } else if (i === actualArr.length - 1) {
