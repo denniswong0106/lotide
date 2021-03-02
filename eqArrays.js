6e8d464547c0ccffa21f55b3f870d65ef136e431
Dennis Wong <denniswong0106@gmail.com>
2021-03-02 22:23:36 +0000

    added functionality for eqArrays to compare two arrays and tell me if they are equivalent or not.

--- eqArrays.js
+++ eqArrays.js
@@ -1,24 +1,17 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🤟🤟🤟Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😡😡😡Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = (actualArr, expectedArr) => {
-  if (actualArr.length === 0 && expectedArr.length === 0) {
-    console.log(`🤟🤟🤟Assertion Passed: Empty === Empty`);
-  } else {
-    for (let i = 0; i < actualArr.length; i++) {
-      if (actualArr[i] !== expectedArr[i]) {
-        console.log(`😡😡😡Assertion Failed: at index ${i}, ${actualArr[i]} !== ${expectedArr[i]}`);
-        break;
-        // so if the array has 50 wrong comparisons it won't just keep going.
-        // stops code running once first error is detected (leftmost side).
-      } else if (i === actualArr.length - 1) {
-        console.log(`🤟🤟🤟Assertion Passed: ${actualArr} === ${expectedArr}`);
-      }
+  for (let i = 0; i < actualArr.length; i++) {
+    if (actualArr[i] !== expectedArr[i]) {
+      console.log(false);
+      return false;
     }
   }
-};
+  console.log(true);
+  return true;
+}
+
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+eqArrays([3, 4,], []);
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
