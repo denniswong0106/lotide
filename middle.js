5fc763438fc0b6630918a056af3e1badf5b4efd1
Dennis Wong <denniswong0106@gmail.com>
2021-03-03 20:17:15 +0000

    added file middle.js which has function that finds the single (odd) or double (even) elements. Returns empty when given array of only 0, 1, or 2 elements.

---
+++ middle.js
@@ -0,0 +1,38 @@
+const eqArrays = (actualArr, expectedArr) => {
+  if (actualArr.length !== expectedArr.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actualArr.length; i++) {
+      if (actualArr[i] !== expectedArr[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (actualArr, expectedArr) => {
+  if (eqArrays(actualArr, expectedArr)) {
+    console.log(`🤟🤟🤟Assertion Passed: ${actualArr} === ${expectedArr}`);
+  } else {
+    console.log(`😡😡😡Assertion Failed: ${actualArr} !== ${expectedArr}`);
+  }
+};
+
+const middle = arr => {
+  let midpoint = Math.floor(arr.length / 2);
+  if (arr.length < 3) {
+    return [];
+  } else if (arr.length % 2 === 1) {
+    return [arr[midpoint]];
+  } else {
+    return [arr[midpoint - 1], arr[midpoint]];
+  }
+};
+
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
