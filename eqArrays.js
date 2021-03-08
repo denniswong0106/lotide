2f858b76f0736dcfc05a869516eef5de964e1924
Dennis Wong <denniswong0106@gmail.com>
2021-03-08 02:11:20 +0000

    refactored eqArrays.js so that the function can handle nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,20 +1,29 @@
 const eqArrays = (actualArr, expectedArr) => {
+  let output = true;
   if (actualArr.length !== expectedArr.length) {
     return false;
   } else {
-    for (let i = 0; i < actualArr.length; i++) {
-      if (actualArr[i] !== expectedArr[i]) {
-        return false;
+    for (const e in actualArr) {
+      if (Array.isArray(actualArr[e])) {
+        output = eqArrays(actualArr[e],expectedArr[e]);
+      } else {
+        if (actualArr[e] !== expectedArr[e]) {
+          return false;
+        }
       }
     }
   }
-  return true;
+  return output;
 };
 
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));// => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-console.log(eqArrays([], [1, 2, 3]));
-console.log(eqArrays([], []));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+// console.log(eqArrays([1,2,3], [1,2,3]));
+// console.log(eqArrays([1, 2, 3], [1, 2, 3]));// => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+// console.log(eqArrays([], [1, 2, 3]));
+// console.log(eqArrays([], []));
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+console.log(eqArrays(['a','b',[1, 2, 3, [true, false]], 'help'], ['a','b',[1, 2, 3, [true, false]], 'help'])) // true
+console.log(eqArrays(['a','b',[1, 2, 3, [true], false], 'help'], ['a','b',[1, 2, 3, [true, false]], 'help'])) // false
+console.log(eqArrays(['a','b',[1, 2, 3, [true, false]]], ['a','b',[1, 2, 3, [true, false]]])) // true
+console.log(eqArrays(['a','b',1, 2, 3, [true, false]], ['a','b',[1, 2, 3, [true, false]]])) // false
